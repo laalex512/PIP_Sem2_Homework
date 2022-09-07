cc8420f0689ca063acd9bfda17a219bed0212afb
laalex <laalex512@gmail.com>
2022-09-07 20:39:50 +0300

    Add Task 15

--- tasks.py
+++ tasks.py
@@ -1,14 +1,25 @@
+# Task 14
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
 
-num = input("Insert Num: ")
-sum = 0
-for i in range(len(num)):
+# num = input("Insert Num: ")
+# sum = 0
+# for i in range(len(num)):
+#     if (num[i] != ",") and (num[i] != "."):
+#         sum += int(num[i])
+# print(sum)
 
-    if (num[i] != ",") and (num[i] != "."):
-        sum += int(num[i])
 
+# Task 15
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-print(sum)
+num = int(input("Insert N: "))
+resultList = [None]*num
+resultList[0] = 1
+for i in range(1, num):
+    resultList[i] = resultList[i-1]*(i+1)
+print(resultList)
